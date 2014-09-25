f670ba8e9b8bdebec239317d3d6a2cc6458ab98f
Michal Stochmialek <mst@avaleo.net>
2014-09-25 17:15:52 +0200

    better mock data

--- server-x/src/main/java/net/avaleo/hackathon/controllers/CaseFlowMessagingWebSocket.java
+++ server-x/src/main/java/net/avaleo/hackathon/controllers/CaseFlowMessagingWebSocket.java
@@ -2,13 +2,19 @@ package net.avaleo.hackathon.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.avaleo.hackathon.dao.HackEsperDaoInterface;
+import java.util.TimeZone;
+
 import net.avaleo.hackathon.events.Event;
 import net.avaleo.hackathon.listener.SocketListener;
+
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 /**
  * Created by ras on 25-09-14.
@@ -24,15 +30,19 @@ public class CaseFlowMessagingWebSocket extends TextWebSocketHandler {
         listener.setSession(session);
         webSocketDao.registerListener(listener);
         super.handleTextMessage(session, message);
-        Event event = new Event();
-        event.setMessage("This is the body part of the message!");
-        event.setSender("Georg Holstrup");
-        event.setTimestamp(new DateTime());
-        ObjectMapper mapper = new ObjectMapper();
-        String msg = mapper.writeValueAsString(event);
-        TextMessage reply = new TextMessage(msg);
+//        Event event = new Event();
+//        event.setMessage("This is the body part of the message!");
+//        event.setSender("Georg Holstrup");
+//        event.setTimestamp(new DateTime());
+//        ObjectMapper mapper = new ObjectMapper();
+//        String msg = mapper.writeValueAsString(event);
+//        TextMessage reply = new TextMessage(msg);
+
+		ObjectMapper mapper = getObjectMapper();
 
         while (true) {
+			String msg = mapper.writeValueAsString(getEvent());
+			TextMessage reply = new TextMessage(msg);
             session.sendMessage(reply);
 
             try {
@@ -42,4 +52,19 @@ public class CaseFlowMessagingWebSocket extends TextWebSocketHandler {
             }
         }
     }
+
+	private ObjectMapper getObjectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setTimeZone(TimeZone.getDefault());
+		mapper.registerModule(new JodaModule());
+		return mapper;
+	}
+
+	private Event getEvent() {
+		Event event = new Event();
+		event.setMessage("This is the body part of the message (" + new DateTime().getSecondOfDay() + ")!");
+		event.setSender("Georg Holstrup");
+		event.setTimestamp(new DateTime());
+		return event;
+	}
 }
