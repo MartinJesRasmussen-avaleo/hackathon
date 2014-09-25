1fb7b23549e8961df2776171a8eb1060308433d2
MartinJesRasmussen-avaleo <17faspb!>
2014-09-25 14:27:50 +0200

    #HACK-1

---
+++ server-x/src/test/java/net/avaleo/hackathon/server/TomcatRunner.java
@@ -0,0 +1,27 @@
+package net.avaleo.hackathon.server;
+
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
+/**
+ * Created by ras on 25-09-14.
+ */
+public class TomcatRunner {
+
+
+
+    public static void main(String[] args) throws Exception {
+        final String webappDirLocation = "src/main/webapp/";
+        final Tomcat tomcat = new Tomcat();
+
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/receipt2erp", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+    }
+}
